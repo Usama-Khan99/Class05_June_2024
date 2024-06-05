1d8532ef8d321063d3f270916ea62ec5cb2bb6b3
Usama-Khan99 <65035298+Usama-Khan99@users.noreply.github.com>
2024-06-05 21:14:15 +0500

    For Loop

--- main.ts
+++ main.ts
@@ -26,7 +26,22 @@ let matrix : number [] [] = [
 
 console.log(matrix[2][1]);
 
+// Home Wrok  "Tuples (is m array ka specific sixe/length btana parhta h"
 
 
+// Loops 
+// For loop  (use when we have to do one work multiple times)
+// round brackets m 3 cheexy dena laxmi h, 1 statement, 2 expression, 3 actual action)
+// yaha py i ko iterator kehty h
+console.log('START');
 
+for (let i = 0; i < 10; i++){
 
+    console.log("Hello World");
+    
+}
+console.log('EXIT');
+
+// 1 iteration m pehly statement chalyga phr 2 expression chalygi or agr expression true hui tou
+// action perform nh hoga wo direct code run py chala jayga phr dusri bari m sbb sy pehly action perform hoga jou k
+// iterator m 1 + kar dyga
