d1196f24b37239eaa9fa7d01384435fc63d3fbc2
Usama-Khan99 <65035298+Usama-Khan99@users.noreply.github.com>
2024-06-05 20:03:48 +0500

    This is array revision

---
+++ main.js
@@ -0,0 +1,9 @@
+// Revision of arrays
+// pop (remove last element from array)
+// push (add element in last)
+// shift (remove first element)
+// unshift (add element in fist place)
+// Slice 
+// Splice
+var grocery = ['Atta', 'Chawal', 'Cheeni'];
+console.log(grocery[0]);
---
+++ main.ts
@@ -0,0 +1,23 @@
+// Revision of arrays
+
+// pop (remove last element from array)
+// push (add element in last)
+// shift (remove first element)
+// unshift (add element in fist place)
+// Slice 
+// Splice
+let grocery = ['Atta','Chawal','Cheeni'];
+
+grocery[0] = '1 kg Atta'; // Overwrite array variable
+grocery[1] = '5 kg chaawal'; 
+grocery[2] = '3 kg cheeni';
+
+console.log(grocery.length);
+
+console.log(grocery[grocery.length-1]); // uSE TO FIND THE LAST ELEMENT OF arrray
+
+
+
+
+
+
